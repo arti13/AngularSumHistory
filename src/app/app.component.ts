bc9914c69b4b2587f79b01a317b72e53063d66ab
Arti Mumbarkar <aartimum@gmail.com>
2025-09-22 17:11:19 +0530

    Adding Sum History Angular App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { FormsModule } from '@angular/forms'; 
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'SumHistoryNumber';
+  
+  num1: number = 0;
+  num2: number = 0;
+  result: number | null = null;
+  history: number[] = [];
+
+
+  calculateSum() {
+    this.result = this.num1 + this.num2;
+    this.history.push(this.result);
+  }
+
 }
